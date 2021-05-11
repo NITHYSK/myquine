de5f6ef1d51c49a89be0d79128014edd382e4f01
HYSK <31800393+NITHYSK@users.noreply.github.com>
2021-05-11 17:28:57 +0900

    Delete main.c

--- main.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main() {
-	char progdata[] =
-	    "#include <stdio.h>%cint main(){char progdata[] = "
-	    "%c%s%c;printf(progdata,10,34,progdata,34);}";
-	printf(progdata, 10, 34, progdata, 34);
-}
